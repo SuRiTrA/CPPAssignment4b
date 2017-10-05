95bcaac225fc3b9f058d814002f698e3e5acbfc6
Suritra Bandyopadhyay <31152879+SurisGithub@users.noreply.github.com>
2017-10-05 23:51:07 +0530

    Update CPP13.cpp

--- CPP13.cpp
+++ CPP13.cpp
@@ -1,1 +1,31 @@
-# CPPAssignment4b
+#include <iostream>
+using namespace std;
+//C++ program to display all array elements using recursion.
+
+int printfun(int num[],int c,int size)
+{
+	if(c<size)
+  {
+		cout<<num[c]<<endl;
+		display(num,++c,size);
+	}
+}
+
+int main()
+{
+  int size;
+  cout<<"C++ program to display all array elements using recursion";
+  cout<<"\n \nEnter the size of array: ";
+  cin>>size;
+  int arr[size];
+  for(int i=0;i<size;i++)
+  {
+		cout<<"Enter your element:";
+		cin>>arr[i];
+	}
+  printfunc(arr,0,size);
+  return 0;
+}
+   
+
+
