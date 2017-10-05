49f0a774150f1565aecf036c1b1a4d8c21d064be
Suritra Bandyopadhyay <31152879+SurisGithub@users.noreply.github.com>
2017-10-06 00:29:27 +0530

    Update CPP12.cpp

--- CPP12.cpp
+++ CPP12.cpp
@@ -1,1 +1,26 @@
-# CPPAssignment4b
+#include <iostream>
+using namespace std;
+#include <
+
+//Program to find LCM of two numbers using recurssions
+
+int main()
+{
+  void gcd(int num1,int num2,int c){
+	if(c%num1==0 && c%num2==0)
+		cout<<"HCF of the 2 numbers:"<<c;
+	else	
+		gcd(num1,num2,++c);
+}
+ 
+int main() {
+	int num1,num2,c;
+  cout<<"\n \nProgram to find LCM of two numbers using recurssions";
+	cout<<"\nEnter First Number";
+	cin>>num1;
+	cout<<"Enter Second Number";
+	cin>>num2;
+	c=max(num1,num2);
+	gcd(num1,num2,c);
+	return 0;
+}
